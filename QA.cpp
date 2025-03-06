97c45d9bd8c264ddd82898ffd3cc3b31e2e36351
Jordan Sowell <jordanscompsci@gmail.com>
2025-03-06 13:56:05 -0500

    Fixed the shuffle, now Have to fix the answer checking for the quiz

--- QA.cpp
+++ QA.cpp
@@ -17,34 +17,32 @@ int QA::getScore(){
 int QA::letterCount(){
     int questionCounter = 0;
     int answerCounter = 0;
-    for(int i = 0; i < question.size(); i++){
-        if(isalpha(question.at(i))){
+    for(int i = 0; i < question.size() - 1; i++){
+        if(answer.at(i) != '\n' && answer.at(i) != ' '){
             questionCounter++;
         }
     }
-    for(int i = 0; i < answer.size(); i++){
-        if(isalpha(answer.at(i))){
+    //std::cout << "Question Size: " << questionCounter << std::endl; // test for question
+    for(int i = 0; i < answer.size() - 1; i++){
+        if(answer.at(i) != '\n' && answer.at(i) != ' '){
             answerCounter++;
         }
     }
-    return (questionCounter + answerCounter);
+    //std::cout << "Answer Size: " << answerCounter << std::endl; // test for question
+
+    return questionCounter;
 }
 
 void QA::updateScore(int val){ // Figure out what to do if score equals 0
-    if(val > 0){
-        score += val;
-    }
-    else if(val < 0){
-        // Able to handle negative values
-        // This runs when the score is not 0
-        score += val; //Using += because if its a negative number it's just going to be switched to positive
-        if(score < 0){
-            score = 0;
-        }
+    // Able to handle negative values
+    // This runs when the score is not 0
+    score += val; //Using += because if its a negative number it's just going to be switched to positive
+    if(score < 0){
+        score = 0;
     }
 }
 
 std::ostream& operator<<(std::ostream& os, const QA& qa) { 
-    os << "Question:" << qa.question << std::endl << " Answer: " << qa.answer;
+    os << qa.question << std::endl << qa.answer;
     return os;
 }
--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-
-/*    QA Test; // Testing update score function
+/*
+    QA Test; // Testing update score function
     cout << "Testing QA update score" << endl;
     Test.updateScore(-1);
     cout << "Score: " << Test.getScore() << endl;
@@ -21,7 +21,8 @@ int main(int argc, char *argv[]){
     ifstream fp; 
     int score; 
     string question;
-    string answer;
+    string correctAnswer;
+    string userAnswer;
     vector<QA> quizVector;
     
     fp.open(argv[1]);
@@ -29,33 +30,48 @@ int main(int argc, char *argv[]){
         cout << "File: " << argv[1] << " could not be opened" << endl;
     }
     cout << "File was opened" << endl << endl;
-
+    if(fp.eofbit)
     while(!fp.eof()){ // Reads in the contents of the database.txt file
         getline(fp, question);
-        getline(fp, answer);
-        QA quiz(question,answer);
+        getline(fp, correctAnswer);
+        QA quiz(question,correctAnswer);
         quizVector.push_back(quiz);
     }
-     // displays all the questions and answers for the vector
-    cout << "Before Sort: " << endl << endl;
-    for(int i = 0; i < quizVector.size(); i++){
-        cout << quizVector[i] << endl;
-    }
-    
     //Step 3
     //Randomly select a question and answer pair
-    random_shuffle:srand(unsigned(time(0)));
+    srand(unsigned(time(0)));
     random_shuffle(quizVector.begin(), quizVector.end());
-    cout << "After Sort: " << endl << endl;
-    for(int i = 0; i < quizVector.size(); i++){
-        cout << quizVector[i] << endl;
+
+    cout << endl << "After Sort: " << endl << endl;
+    for(int i = 0; i < quizVector.size(); ++i){
+        cout << quizVector[i].getQuestion() << endl;
+        cout << "Type your answer: ";
+        cin.ignore();
+        
+
+        if(userAnswer == quizVector[i].getAnswer()){
+            cout << "Correct!";
+            quizVector[i].updateScore(1);
+            cout << "Current Score: " << quizVector[i].getScore() << endl << endl;
+        }else if(userAnswer != quizVector[i].getAnswer()){
+            cout << "Wrong Answer! Correct Answer: " << quizVector[i].getAnswer() << endl;
+            quizVector[i].updateScore(-1);
+            cout << "Current Score: " << quizVector[i].getScore() << endl << endl;
+        }
     }
+    
+    cout << "Number of Questions: " << quizVector.size() - 1 << endl;
+    
     // allows random shuffle 
     //Show the question
     //Collect the input
     //Display the results
     
-    cout << "Closing file" << endl;
+    cout << "Closing file!" << endl;
     fp.close();
     return 0;
 }
+
+/*
+if lastchar of string == 
+*/
